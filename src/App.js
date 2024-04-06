a2a7bd3893b8be32a6fec4194ccdbf0b482e4267
Sorigh <84138511+Sorigh12@users.noreply.github.com>
2024-04-06 02:05:25 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TreeView from './component';
+
+function App() {
+  return (
+    <div className="App">
+      <TreeView menus={menus}/>
+    </div>
+  );
+}
+
+export default App;
